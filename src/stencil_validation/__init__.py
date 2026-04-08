097f8d427b2c10f17a6f38789895eaec010c20a8
stubbiali <subbiali@phys.ethz.ch>
2026-04-08 16:19:56 +0200

    Expose public symbols

--- src/stencil_validation/__init__.py
+++ src/stencil_validation/__init__.py
@@ -17,6 +17,72 @@
 # specific language governing permissions and limitations
 # under the License.
 
+from stencil_validation.config import Config
+from stencil_validation.descriptors import (
+    BaseField,
+    Bool,
+    CompositeField,
+    ConcretizedDescriptor,
+    ConcretizedDescriptorDict,
+    Descriptor,
+    DescriptorDict,
+    Field,
+    Float,
+    Int,
+    concretize,
+    to_file,
+)
+from stencil_validation.dims import Dim, IndexedDim, SizedDim
+from stencil_validation.iox import HDF5Operator, IOFileOperator, NetCDFOperator, io_file_operator
+from stencil_validation.stencil_fortran.descriptors import CompositeFortranField, FortranField
+from stencil_validation.stencil_fortran.stencil import (
+    FortranStencil,
+    MetaFortranStencil,
+    get_fortran_stencil,
+    print_fortran_stencil_list,
+)
+from stencil_validation.stencil_gt4py.descriptors import CompositeGT4PyField, GT4PyField
+from stencil_validation.stencil_gt4py.stencil import (
+    GT4PyStencil,
+    MetaGT4PyStencil,
+    get_gt4py_stencil,
+    print_gt4py_stencil_list,
+)
+
 __version__ = "0.1.0.dev"
 
-__all__ = ["__version__"]
+__all__ = [
+    "BaseField",
+    "Bool",
+    "CompositeField",
+    "CompositeFortranField",
+    "CompositeGT4PyField",
+    "ConcretizedDescriptor",
+    "ConcretizedDescriptorDict",
+    "Config",
+    "Descriptor",
+    "DescriptorDict",
+    "Dim",
+    "Field",
+    "Float",
+    "FortranField",
+    "FortranStencil",
+    "GT4PyField",
+    "GT4PyStencil",
+    "HDF5Operator",
+    "IOFileOperator",
+    "IndexedDim",
+    "Int",
+    "MetaFortranStencil",
+    "MetaGT4PyStencil",
+    "NetCDFOperator",
+    "SizedDim",
+    "__version__",
+    "concretize",
+    "get_fortran_stencil",
+    "get_gt4py_stencil",
+    "io_file_operator",
+    "print_fortran_stencil_list",
+    "print_gt4py_stencil_list",
+    "to_file",
+]
