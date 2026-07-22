2bc2c463ef7209f4babd6b84f47040c293e838f6
stubbiali <subbiali@phys.ethz.ch>
2026-07-22 14:58:54 +0200

    Fix comparison for files that do not share any field

--- src/stencil_validation/validation.py
+++ src/stencil_validation/validation.py
@@ -72,52 +72,53 @@ def compare_io_files(
                 if src_keys_only or trg_keys_only:
                     print("")
 
-                max_key_len = max(len(key) for key in common_keys)
-                for key in common_keys:
-                    src_field = src_file_op.get_field(key)[  # type: ignore[index]
-                        index_slices if index_slices is not None else ...
-                    ]
-                    trg_field = trg_file_op.get_field(key)[  # type: ignore[index]
-                        index_slices if index_slices is not None else ...
-                    ]
-                    assert src_field.shape == trg_field.shape
+                if len(common_keys) > 0:
+                    max_key_len = max(len(key) for key in common_keys)
+                    for key in common_keys:
+                        src_field = src_file_op.get_field(key)[  # type: ignore[index]
+                            index_slices if index_slices is not None else ...
+                        ]
+                        trg_field = trg_file_op.get_field(key)[  # type: ignore[index]
+                            index_slices if index_slices is not None else ...
+                        ]
+                        assert src_field.shape == trg_field.shape
 
-                    if src_field.dtype.kind == "b":
-                        src_field = src_field.astype(float)
-                    if trg_field.dtype.kind == "b":
-                        trg_field = trg_field.astype(float)
+                        if src_field.dtype.kind == "b":
+                            src_field = src_field.astype(float)
+                        if trg_field.dtype.kind == "b":
+                            trg_field = trg_field.astype(float)
 
-                    # remove nan's and inf's
-                    src_field = np.where(np.isnan(src_field), 0, src_field)
-                    src_field = np.where(np.isinf(src_field), 0, src_field)
-                    trg_field = np.where(np.isnan(trg_field), 0, trg_field)
-                    trg_field = np.where(np.isinf(trg_field), 0, trg_field)
+                        # remove nan's and inf's
+                        src_field = np.where(np.isnan(src_field), 0, src_field)
+                        src_field = np.where(np.isinf(src_field), 0, src_field)
+                        trg_field = np.where(np.isnan(trg_field), 0, trg_field)
+                        trg_field = np.where(np.isinf(trg_field), 0, trg_field)
 
-                    abs_diff = np.abs(src_field - trg_field)
-                    abs_diff_max = abs_diff.max()
-                    with warnings.catch_warnings():
-                        warnings.filterwarnings("ignore")
-                        rel_diff = abs_diff / np.abs(trg_field)
-                    rel_diff_max = np.where(trg_field != 0, rel_diff, 0).max()
+                        abs_diff = np.abs(src_field - trg_field)
+                        abs_diff_max = abs_diff.max()
+                        with warnings.catch_warnings():
+                            warnings.filterwarnings("ignore")
+                            rel_diff = abs_diff / np.abs(trg_field)
+                        rel_diff_max = np.where(trg_field != 0, rel_diff, 0).max()
 
-                    close = np.abs(src_field - trg_field) <= atol + rtol * np.abs(trg_field)
-                    allclose = np.all(close)
+                        close = np.abs(src_field - trg_field) <= atol + rtol * np.abs(trg_field)
+                        allclose = np.all(close)
 
-                    freq_atol = np.sum(abs_diff >= atol) / abs_diff.size * 100
-                    freq_rtol = np.sum(rel_diff >= rtol) / abs_diff.size * 100
-                    freq_close = np.sum(np.logical_not(close)) / abs_diff.size * 100
+                        freq_atol = np.sum(abs_diff >= atol) / abs_diff.size * 100
+                        freq_rtol = np.sum(rel_diff >= rtol) / abs_diff.size * 100
+                        freq_close = np.sum(np.logical_not(close)) / abs_diff.size * 100
 
-                    print(
-                        f"   {key:{max_key_len}s}:"
-                        f"\033[9{2 if abs_diff_max < atol else 1}m "
-                        f"max abs diff = {abs_diff_max:.5E} "
-                        f"({f'{freq_atol:.2f}'.zfill(5)} %)\033[00m,"
-                        f"\033[9{2 if rel_diff_max < rtol else 1}m "
-                        f"max rel diff = {rel_diff_max:.5E} "
-                        f"({f'{freq_rtol:.2f}'.zfill(5)} %)\033[00m,"
-                        f"\033[9{2 if allclose else 1}m allclose = {allclose} "
-                        f"({f'{freq_close:.2f}'.zfill(5)} %)\033[00m"
-                    )
+                        print(
+                            f"   {key:{max_key_len}s}:"
+                            f"\033[9{2 if abs_diff_max < atol else 1}m "
+                            f"max abs diff = {abs_diff_max:.5E} "
+                            f"({f'{freq_atol:.2f}'.zfill(5)} %)\033[00m,"
+                            f"\033[9{2 if rel_diff_max < rtol else 1}m "
+                            f"max rel diff = {rel_diff_max:.5E} "
+                            f"({f'{freq_rtol:.2f}'.zfill(5)} %)\033[00m,"
+                            f"\033[9{2 if allclose else 1}m allclose = {allclose} "
+                            f"({f'{freq_close:.2f}'.zfill(5)} %)\033[00m"
+                        )
 
                 print("\n== iodiff: end")
 
