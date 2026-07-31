f4bf81a2b7d584a0d6250e52bc92a98a1adcd177
stubbiali <subbiali@phys.ethz.ch>
2026-07-31 15:29:35 +0200

    Improve compare_io_files

--- src/stencil_validation/validation.py
+++ src/stencil_validation/validation.py
@@ -19,12 +19,14 @@
 
 from __future__ import annotations
 
+import functools
 import warnings
 
 import click
 import numpy as np
 
 from stencil_validation.iox import io_file_operator
+from stencil_validation.utils import printx
 
 ATOL = 1e-12
 RTOL = 1e-10
@@ -44,21 +46,22 @@ def compare_io_files(
     rtol: float | None = RTOL,
     verbose: bool = False,
 ) -> None:
+    print_ = functools.partial(printx, flush=True, verbose=True)
     with io_file_operator(src_file_path, mode="r", verbose=verbose) as src_file_op:
         with io_file_operator(trg_file_path, mode="r", verbose=verbose) as trg_file_op:
             if src_file_op is None:
-                print(f"== iodiff: cannot open `{src_file_path}`")
+                print_(f"== iodiff: cannot open `{src_file_path}`")
             elif trg_file_op is None:
-                print(f"== iodiff: cannot open `{trg_file_path}`")
+                print_(f"== iodiff: cannot open `{trg_file_path}`")
             else:
                 atol = atol or ATOL
                 rtol = rtol or RTOL
 
-                print("== iodiff: start\n")
-                print(f"   - source file: {src_file_op.f_path}")
-                print(f"   - target file: {trg_file_op.f_path}")
-                print(f"   - atol: {atol:.1E}")
-                print(f"   - rtol: {rtol:.1E}\n")
+                print_("== iodiff: start\n")
+                print_(f"   - source file: {src_file_op.f_path}")
+                print_(f"   - target file: {trg_file_op.f_path}")
+                print_(f"   - atol: {atol:.1E}")
+                print_(f"   - rtol: {rtol:.1E}\n")
 
                 common_keys = sorted(
                     (src_keys := {*src_file_op.field_names}).intersection(
@@ -66,60 +69,72 @@ def compare_io_files(
                     )
                 )
                 if src_keys_only := (src_keys - {*common_keys}):
-                    print(f"   - fields found only in source file: {','.join(src_keys_only)}")
+                    print_(f"   - fields found only in source file: {','.join(src_keys_only)}")
                 if trg_keys_only := (trg_keys - {*common_keys}):
-                    print(f"   - fields found only in target file: {','.join(trg_keys_only)}")
-                if src_keys_only or trg_keys_only:
-                    print("")
+                    print_(f"   - fields found only in target file: {','.join(trg_keys_only)}")
 
-                max_key_len = max(len(key) for key in common_keys)
-                for key in common_keys:
-                    src_field = src_file_op.get_field(key)[  # type: ignore[index]
-                        index_slices if index_slices is not None else ...
-                    ]
-                    trg_field = trg_file_op.get_field(key)[  # type: ignore[index]
-                        index_slices if index_slices is not None else ...
-                    ]
-                    assert src_field.shape == trg_field.shape
+                if len(common_keys) > 0:
+                    if src_keys_only or trg_keys_only:
+                        print_("")
 
-                    if src_field.dtype.kind == "b":
-                        src_field = src_field.astype(float)
-                    if trg_field.dtype.kind == "b":
-                        trg_field = trg_field.astype(float)
+                    max_key_len = max(len(key) for key in common_keys)
+                    for key in common_keys:
+                        src_field = src_file_op.get_field(key)[  # type: ignore[index]
+                            index_slices if index_slices is not None else ...
+                        ]
+                        trg_field = trg_file_op.get_field(key)[  # type: ignore[index]
+                            index_slices if index_slices is not None else ...
+                        ]
 
-                    # remove nan's and inf's
-                    src_field = np.where(np.isnan(src_field), 0, src_field)
-                    src_field = np.where(np.isinf(src_field), 0, src_field)
-                    trg_field = np.where(np.isnan(trg_field), 0, trg_field)
-                    trg_field = np.where(np.isinf(trg_field), 0, trg_field)
+                        if src_field.dtype.kind == "S" and trg_field.dtype.kind == "S":
+                            print_(f"   {key:{max_key_len}s}: string")
+                            continue
 
-                    abs_diff = np.abs(src_field - trg_field)
-                    abs_diff_max = abs_diff.max()
-                    with warnings.catch_warnings():
-                        warnings.filterwarnings("ignore")
-                        rel_diff = abs_diff / np.abs(trg_field)
-                    rel_diff_max = np.where(trg_field != 0, rel_diff, 0).max()
+                        if src_field.shape != trg_field.shape:
+                            print_(
+                                f"   {key:{max_key_len}s}: incompatible shapes {src_field.shape} "
+                                f"and {trg_field.shape}"
+                            )
+                            continue
 
-                    close = np.abs(src_field - trg_field) <= atol + rtol * np.abs(trg_field)
-                    allclose = np.all(close)
+                        if src_field.dtype.kind == "b":
+                            src_field = src_field.astype(float)
+                        if trg_field.dtype.kind == "b":
+                            trg_field = trg_field.astype(float)
 
-                    freq_atol = np.sum(abs_diff >= atol) / abs_diff.size * 100
-                    freq_rtol = np.sum(rel_diff >= rtol) / abs_diff.size * 100
-                    freq_close = np.sum(np.logical_not(close)) / abs_diff.size * 100
+                        # remove nan's and inf's
+                        src_field = np.where(np.isnan(src_field), 0, src_field)
+                        src_field = np.where(np.isinf(src_field), 0, src_field)
+                        trg_field = np.where(np.isnan(trg_field), 0, trg_field)
+                        trg_field = np.where(np.isinf(trg_field), 0, trg_field)
 
-                    print(
-                        f"   {key:{max_key_len}s}:"
-                        f"\033[9{2 if abs_diff_max < atol else 1}m "
-                        f"max abs diff = {abs_diff_max:.5E} "
-                        f"({f'{freq_atol:.2f}'.zfill(5)} %)\033[00m,"
-                        f"\033[9{2 if rel_diff_max < rtol else 1}m "
-                        f"max rel diff = {rel_diff_max:.5E} "
-                        f"({f'{freq_rtol:.2f}'.zfill(5)} %)\033[00m,"
-                        f"\033[9{2 if allclose else 1}m allclose = {allclose} "
-                        f"({f'{freq_close:.2f}'.zfill(5)} %)\033[00m"
-                    )
+                        abs_diff = np.abs(src_field - trg_field)
+                        abs_diff_max = abs_diff.max()
+                        with warnings.catch_warnings():
+                            warnings.filterwarnings("ignore")
+                            rel_diff = abs_diff / np.abs(trg_field)
+                        rel_diff_max = np.where(trg_field != 0, rel_diff, 0).max()
 
-                print("\n== iodiff: end")
+                        close = np.abs(src_field - trg_field) <= atol + rtol * np.abs(trg_field)
+                        allclose = np.all(close)
+
+                        freq_atol = np.sum(abs_diff >= atol) / abs_diff.size * 100
+                        freq_rtol = np.sum(rel_diff >= rtol) / abs_diff.size * 100
+                        freq_close = np.sum(np.logical_not(close)) / abs_diff.size * 100
+
+                        print_(
+                            f"   {key:{max_key_len}s}:"
+                            f"\033[9{2 if abs_diff_max < atol else 1}m "
+                            f"max abs diff = {abs_diff_max:.5E} "
+                            f"({f'{freq_atol:.2f}'.zfill(5)} %)\033[00m,"
+                            f"\033[9{2 if rel_diff_max < rtol else 1}m "
+                            f"max rel diff = {rel_diff_max:.5E} "
+                            f"({f'{freq_rtol:.2f}'.zfill(5)} %)\033[00m,"
+                            f"\033[9{2 if allclose else 1}m allclose = {allclose} "
+                            f"({f'{freq_close:.2f}'.zfill(5)} %)\033[00m"
+                        )
+
+                print_("\n== iodiff: end")
 
 
 if __name__ == "__main__":
